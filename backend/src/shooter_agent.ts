73e4a056ae00cecefc7d7e0c838dc7bb6205b967
basimkhajwal <basimkhajwal@gmail.com>
2019-11-16 20:31:36 +0000

    Remove symlink and use folder in frontend

--- backend/src/shooter_agent.ts
+++ backend/src/shooter_agent.ts
@@ -1,5 +1,5 @@
-import {Agent, } from "../../frontend/src/shared/interfaces"
-import {ShooterObservation, ShooterAction} from "../../frontend/src/shared/shooter_interfaces"
+import {Agent, } from "interfaces"
+import {ShooterObservation, ShooterAction} from "shooter_interfaces"
 
 export class DumbAgent implements Agent<ShooterObservation, ShooterAction> {
   act(state: ShooterObservation): ShooterAction {
