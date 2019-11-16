ec3508beb759d63df51e6dd93272b6f9feb2dbbc
basimkhajwal <basimkhajwal@gmail.com>
2019-11-16 20:59:22 +0000

    Updated interface.

--- frontend/src/shared/shooter_interfaces.ts
+++ frontend/src/shared/shooter_interfaces.ts
@@ -1,10 +1,31 @@
 import {GameState, GameAction, GameObservation, Agent, Renderer, Game} from "./interfaces"
 
+export const GameOptions = {
+  gameWidth: 600,
+  gameHeight: 400,
+  fps: 30, // so a tick every 1000/30 = 33ms
+
+  playerRadius: 20,
+  playerMoveSpeed: 50,  // pixels / second
+  playerTurnSpeed: 180, // degrees  / second
+
+  sensorRadius: 100,
+
+  bulletSpeed: 500, // pixels / second
+  bulletRadius: 2,
+  bulletDamage: 0.5, // amount of player health removed (starting health is 1)
+  bulletCooldown: 1, // time between bullets
+}
+
 export interface ShooterState extends GameState {
-  width: number;
-  height: number;
-  agents: Array<[number, number, number]>; //(x, y, angle)
+  players: Array<Player>;
   bullets: Array<Bullet>;
+}
+
+export interface Player {
+  x: number,
+  y: number,
+  angle: number,
 }
 
 export interface Bullet {
@@ -18,11 +39,17 @@ export interface ShooterObservation extends GameObservation {
   x: number;
   y: number;
   angle: number;
-  sensors: Array<number>;
+  cooldown: number; // seconds left
+
+  // Sensors are 0 or 1 (indicating presence)
+  enemySensors: Array<number>;
+  bulletSensors: Array<number>;
 }
 
 export interface ShooterAction extends GameAction {
   fireBullet: boolean;
-  angle: number;
-  speed: number;
+  // If both are specified (or none) then don't turn
+  turnLeft: boolean;
+  turnRight: boolean;
+  moveForward: number;
 }
