dfc4749e0dfef12bb8aea34792b1c3e2086baeee
theLAZYmd <lazymeep+github@gmail.com>
2019-11-17 08:50:04 +0000

    Updates

---
+++ precache-manifest.90405fa8f08b5cafbdcfd68aeccd977c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd94096206e840099d01ac2c39c8e08a",
+    "url": "/enders-game/index.html"
+  },
+  {
+    "revision": "6c1bc00c94e029490653",
+    "url": "/enders-game/static/css/main.85b1ac6a.chunk.css"
+  },
+  {
+    "revision": "3c87fe9ed04fd339ad38",
+    "url": "/enders-game/static/js/2.9562b981.chunk.js"
+  },
+  {
+    "revision": "6c1bc00c94e029490653",
+    "url": "/enders-game/static/js/main.8097a658.chunk.js"
+  },
+  {
+    "revision": "c923f9423b723a14ffc4",
+    "url": "/enders-game/static/js/runtime-main.933d4f2b.js"
+  }
+]);
