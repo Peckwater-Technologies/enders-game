58baa189bd4f012a360e01c9a425562605e6ceea
theLAZYmd <lazymeep+github@gmail.com>
2019-11-17 07:27:00 +0000

    Rectangle

--- frontend/src/Canvas.jsx
+++ frontend/src/Canvas.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stage, Layer, Rect} from 'react-konva';
+import { Stage, Layer, Rect, Text } from 'react-konva';
 
 import Shooter from './Objects/Shooter';
 import Bullet from './Objects/Bullet';
@@ -37,9 +37,9 @@ export default class Canvas extends React.Component {
 
 	updateState(ShooterState) {
 		let state = Object.assign(this.state, ShooterState);
-		let [actual, minX, minY] = this.getScale(state);
+		let [actual, centreX, centreY, rangeX, rangeY] = this.getScale(state);
 		state = Object.assign(state, {
-			scale: (this.state.scale + actual) / 2,
+			scale: Math.min(Math.min((this.state.scale + actual) / 2), GameOptions.gameWidth / config.minWidth),
 			minX, minY
 		});
 		this.setState(state);
@@ -75,41 +75,52 @@ export default class Canvas extends React.Component {
 	getScale(state) {
 		let xs = state.players.map(b => b.x);
 		let minX = Math.min(...xs);
+		if (isNaN(minX)) minX = 0;
 		let rangeX = Math.max(...xs) - minX;
 		let ys = state.players.map(b => b.y);
 		let minY = Math.min(...ys);
+		if (isNaN(minY)) minY = 0;
 		let rangeY = Math.max(...ys) - minY;
-		let scaleX = state.surface.width / (rangeX + 20);
-		let scaleY = state.surface.height / (rangeY + 20);
+		let scaleX = GameOptions.gameWidth / (rangeX + 20);
+		let scaleY = GameOptions.gameHeight / (rangeY + 20);
 		//accounts for limits of zoom out
-		//let centreX = b.x + 0.5 + rangeX;
-		//let centreY = b.y + 0.5 + rangeY;
+		let centreX = rangeX * 0.5 + minX;
+		let centreY = rangeY * 0.5 + minY;
 		let scale = Math.max(Math.min(scaleX, scaleY), state.surface.width / GameOptions.gameWidth, state.surface.height / GameOptions.gameHeight);
 		if (isNaN(scale)) scale = 1;
-		return [scale, Math.max(minX - 20, 0), Math.max(minY - 20, 0)];
+		return [scale, centreX, centreY, rangeX, rangeY];
 	}
 
 	render() {
-		console.log(this.state.minX, this.state.minY);
-		return <Stage {...this.state.surface} x={this.state.minX} y={this.state.minY} scale={{
-			x: this.state.scale,
-			y: this.state.scale,
-			width: this.state.surface.width,
-			height: this.state.surface.height
-		}}>
+		let theoreticalX = GameOptions.gameWidth / this.state.scale;
+		let calculatedXOffset = Math.min(this.state.minX, GameOptions.gameWidth - theoreticalX);
+		let theoreticalY = GameOptions.gameHeight / this.state.scale;
+		let calculatedYOffset = Math.min(this.state.minY, GameOptions.gameHeight - theoreticalY);
+		
+		console.log(theoreticalX, theoreticalY, this.state.scale);
+		return <Stage {...this.state.surface} /*
+			x={-calculatedXOffset}
+			y={-calculatedYOffset}
+			scale={{
+				x: this.state.scale,
+				y: this.state.scale
+			}}*/
+		>
 			<Layer id='background'>				
 				<Rect
-					x={background.x}
-					y={background.y}
-					width={Math.min(this.state.surface.width / this.state.scale, GameOptions.gameWidth)}
-					height={Math.min(this.state.surface.height / this.state.scale, GameOptions.gameHeight)}
+					width={GameOptions.gameWidth}
+					height={GameOptions.gameHeight}
+					//width={theoreticalX}
+					//height={theoreticalY}
 					fill={background.colour}
 				/>
 			</Layer>
 			<Layer id='grid'>				
 				<Grid
-					width={Math.min(this.state.surface.width / this.state.scale, GameOptions.gameWidth)}
-					height={Math.min(this.state.surface.height / this.state.scale, GameOptions.gameHeight)}
+					width={GameOptions.gameWidth}
+					height={GameOptions.gameHeight}
+					//width={theoreticalX}
+					//height={theoreticalY}
 					freq={config.grid.freqWidth}
 				/>
 			</Layer>
@@ -127,6 +138,21 @@ export default class Canvas extends React.Component {
 					key = {['bullet', i].join('.')}
 					data={b}
 				/>)}
+			</Layer>
+			<Layer>
+				<Rect
+					width={theoreticalX}
+					height={theoreticalY}
+					x={calculatedXOffset}
+					y={calculatedYOffset}
+					strokeWidth={10}
+					stroke='black'
+				/>
+				<Text x={0} y={0} fontSize={100} text={`
+				Size: (${theoreticalX}, ${theoreticalY})
+				Offset: (${calculatedXOffset}, ${calculatedYOffset})
+				Minimum: (${this.state.minX}, ${this.state.minY})
+				`}/>
 			</Layer>
 		</Stage>
 	}
