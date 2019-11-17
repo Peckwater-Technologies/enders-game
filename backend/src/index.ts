4814d25df6c78715fd6ab34c468f49dd4ddcb285
basimkhajwal <basimkhajwal@gmail.com>
2019-11-17 01:37:21 +0000

    Some progress.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 4321;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+const models = [
+
+ ];
+
+app.get("/models/shooter", (req, res) => {
+  
+});
+
+app.post("/models/shooter", (req, res) => {
+  
+});
+
 // tslint:disable-next-line: no-console
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
