c773b2966f23fd1e94973056055b7779f0ce65c2
janquo <jan.guzik@cs.ox.ac.uk>
2019-11-17 06:36:49 +0000

    ok

--- frontend/src/shared/dumb_bot.ts
+++ frontend/src/shared/dumb_bot.ts
@@ -23,3 +23,41 @@ export class StampedeBot implements Agent<ShooterObservation, ShooterAction> {
     };
   }
 }
+
+export class RealPlayer implements Agent<ShooterObservation, ShooterAction> {
+  rotate_left = false;
+  rotate_right = false;
+  move = false;
+  shoot = false;
+  handleKeyPress(event: React.KeyboardEvent<HTMLDivElement>): void {
+		console.log('hello world');
+		console.log(event);
+		if (event.keyCode === 38) {
+			this.move = !this.move;
+		}
+		else
+		if (event.keyCode === 40) {
+			this.shoot = !this.shoot;
+		}
+		else
+		if (event.keyCode === 37) {
+		   this.rotate_left = !this.rotate_left;
+		}
+		else
+		if (event.keyCode === 39) {
+		   this.rotate_right = !this.rotate_right;
+		}
+	}
+
+  constructor() {
+
+  }
+  act(state: ShooterObservation): ShooterAction {
+    return {
+      fireBullet: this.shoot,
+      turnLeft: this.rotate_left,
+      turnRight: this.rotate_right,
+      moveForward: this.move,
+    };
+  }
+}
